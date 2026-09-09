adf5456b554197c76440530686601c032604978a
GKwoo <go980314@gmail.com>
2026-09-09 23:24:47 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    fac = 1
+    for i in range(1, n+1):
+        fac = fac * i
+        if fac <= n:
+            answer = i
+        else:
+            break
+    return answer
